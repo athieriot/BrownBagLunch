1627e608907ee6b3a8c5b5ddc8332e00654f6c1a
Tugdual Grall <tugdual@gmail.com>
2013-05-07 15:52:45 +0300

    Adding a new bagger

--- js/baggers.js
+++ js/baggers.js
@@ -23,6 +23,27 @@ var data = {
                             summary: "CasperJS is a Browser testing utility for Javascript. Discover how to use it and what makes it different from other similar tools. (advice included)"
                         }
                     ]
+                },
+                {
+                    name: "Tugdual Grall aka 'Tug',
+                    bio: "Technical Evangelist at Couchbase",
+                    picture: "https://secure.gravatar.com/avatar/aab9ac774f61c5d9bf143b5a1bfe901b?s=150",
+                    websites: [
+                        {title: "Blog personnel", href: "http://blog.grallandco.com"}
+                    ],
+                    twitter: "tgrall",
+                    contact: "<a href='mailto:tugdual@gmail.com'>tugdual@gmail.com</a>",
+                    location: "Nantes,France",
+                    sessions: [
+                        {
+                            title: "Introduction to NoSQL with Couchbase",
+                            summary: "This presentation is made of 2 sections: quick introduction to NoSQL, then deep dive into Couchbase Server, the Open Source Document Database: architecture, programming APIs, query using Views/MapReduce.<br/>You can ask as many questions as you want!"
+                        },
+                        {
+                            title: "Big Data and NoSQL... Hadoop and Couchbase",
+                            summary: "See how and why you should integrate Hadoop and Couchbase together and what are the benefits of such integration. I will use some concrete use case to explain this."
+                        }
+                    ]
                 }
             ]
         }
