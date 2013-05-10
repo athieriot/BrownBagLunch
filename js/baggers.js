b4e8bcf08ce6ec1348c481add34da9b7b6a58089
Aurélien Thieriot <a.thieriot@gmail.com>
2013-05-10 17:23:44 +0100

    Move cities

--- js/baggers.js
+++ js/baggers.js
@@ -1,32 +1,6 @@
 var data = {
     villes: [
         {
-            name: "Cardiff",
-            ville_img: "img/BBL_Cardiff.jpg",
-            lat: 51.481581,
-            lng: -3.179090,
-
-            baggers: [
-                {
-                    name: "Aurélien Thieriot",
-                    bio: "Web Developer",
-                    picture: "https://secure.gravatar.com/avatar/0d40e29bf1ef6d51c9b8e6e7a0edd9f5?s=150",
-                    websites: [
-                        {title: "Blog personnel", href: "http://aurelien.thier.io"}
-                    ],
-                    twitter: "a_thieriot",
-                    contact: "<a href='mailto:a.thieriot@gmail.com'>a.thieriot@gmail.com</a>",
-                    location: "Cardiff",
-                    sessions: [
-                        {
-                            title: "Overview of CasperJS",
-                            summary: "CasperJS is a Browser testing utility for Javascript. Discover how to use it and what makes it different from other similar tools. (advice included)"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
             name: "London",
             ville_img: "img/BBL_London.jpg",
             lat: 51.511214,
@@ -72,6 +46,32 @@ var data = {
                         {
                             title: "Big Data and NoSQL... Hadoop and Couchbase",
                             summary: "See how and why you should integrate Hadoop and Couchbase together and what are the benefits of such integration. I will use some concrete use case to explain this."
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "Cardiff",
+            ville_img: "img/BBL_Cardiff.jpg",
+            lat: 51.481581,
+            lng: -3.179090,
+
+            baggers: [
+                {
+                    name: "Aurélien Thieriot",
+                    bio: "Web Developer",
+                    picture: "https://secure.gravatar.com/avatar/0d40e29bf1ef6d51c9b8e6e7a0edd9f5?s=150",
+                    websites: [
+                        {title: "Blog personnel", href: "http://aurelien.thier.io"}
+                    ],
+                    twitter: "a_thieriot",
+                    contact: "<a href='mailto:a.thieriot@gmail.com'>a.thieriot@gmail.com</a>",
+                    location: "Cardiff",
+                    sessions: [
+                        {
+                            title: "Overview of CasperJS",
+                            summary: "CasperJS is a Browser testing utility for Javascript. Discover how to use it and what makes it different from other similar tools. (advice included)"
                         }
                     ]
                 }
